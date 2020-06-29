0c871923283038484f4f2490e7be16f312232d85
yashjain092000 <yashjain092000@gmail.com>
2020-06-29 12:37:15 -0500

    store user's entered value to variables

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -9,6 +9,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +34,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                email=value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText:'Enter your email'),
             ),
@@ -40,7 +43,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                password=value;
               },
              decoration: kTextFieldDecoration.copyWith(hintText:'Enter your password'),
             ),
