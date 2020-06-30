73ff50140092dc779c94dd7f9a7397082f03ec6c
yashjain092000 <yashjain092000@gmail.com>
2020-06-30 17:22:57 -0500

    turn streams to streambuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -32,12 +32,11 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-
-  void messagesStream() async{
-    await for(var snapshot in  _firestore.collection('messages').snapshots()){
- for (var message in snapshot.documents) {
-      print(message.data);
-    }
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
     }
   }
 
@@ -62,6 +61,32 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.lightBlueAccent,
+                      ),
+                    );
+
+                  }
+                    final messages = snapshot.data.documents;
+                    List<Text> messageWidgets = [];
+                    for (var message in messages) {
+                      final messageText = message.data['text'];
+                      final messageSender = message.data['sender'];
+                      final messageWidget =
+                          Text('$messageText from $messageSender');
+                      messageWidgets.add(messageWidget);
+                    }
+                    return Column(
+                      children: messageWidgets,
+                    );
+                  
+                },
+                ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
